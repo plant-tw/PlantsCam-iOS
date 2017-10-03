0cbaba1644de2238b532101958326c35ce6ca598
denkeni <denkenie@gmail.com>
2017-10-03 16:24:38 +0800

    Take photo and get its scale

--- ARSampleApp/ViewController.swift
+++ ARSampleApp/ViewController.swift
@@ -71,6 +71,23 @@ final class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func detectObjects() {
+        // Calculating pixel
+        let startFrameValue = view.center
+        let endFrameValue = CGPoint(x: view.center.x + 100, y: view.center.y)
+        let deltaX = Float(endFrameValue.x - startFrameValue.x)
+        let deltaY = Float(endFrameValue.y - startFrameValue.y)
+        let lengthInPoint = sqrtf(deltaX * deltaX + deltaY * deltaY)          // in point
+        let lengthInPixel = lengthInPoint * Float(UIScreen.main.scale)        // in pixel
+
+        // Calculating length
+        if let startWorldVector = sceneView.realWorldVector(screenPos: startFrameValue),
+            let endWorldVector = sceneView.realWorldVector(screenPos: endFrameValue) {
+            let lengthInMeter = startWorldVector.distance(from: endWorldVector)
+            print("pixel: \(lengthInPixel)")
+            print("centimeter: \(lengthInMeter * 100)")
+            print("\n")
+        }
+
         if let worldPos = sceneView.realWorldVector(screenPos: view.center) {
             aimLabel.isHidden = false
             notReadyLabel.isHidden = true
@@ -88,6 +105,7 @@ final class ViewController: UIViewController, ARSCNViewDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         resetValues()
         measuring = true
+        UIImageWriteToSavedPhotosAlbum(sceneView.snapshot(), nil, nil, nil)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {

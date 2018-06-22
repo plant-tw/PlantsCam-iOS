ba3c721ca07498da60d6085bf5db923cd270685c
denkeni <denkenie@gmail.com>
2018-06-22 11:47:11 +0800

    Improve launching speed for Core ML inference

--- ARSampleApp/ViewController.swift
+++ ARSampleApp/ViewController.swift
@@ -288,9 +288,15 @@ extension ViewController : ARSessionDelegate {
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         // Do not enqueue other buffers for processing while another Vision task is still running.
         // The camera stream has only a finite amount of buffers available; holding too many buffers for analysis would starve the camera.
-        guard currentBuffer == nil, case .normal = frame.camera.trackingState else {
+        guard currentBuffer == nil else {
             return
         }
+        // Uncomment code below when we're doing inference with AR information
+        /*
+        guard case .normal = frame.camera.trackingState else {
+            return
+        }
+         */
 
         // Retain the image buffer for Vision processing.
         self.currentBuffer = frame.capturedImage
